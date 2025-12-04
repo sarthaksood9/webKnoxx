d1d9e1a11fdf3e64e014adc7c270ace7faaaccdc
sarthaksood9 <sarthaksood9@gmail.com>
2025-12-05 00:00:17 +0530

    fix icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
@@ -11,12 +12,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'WebView',
+          tabBarIcon: ({ color, size }) => <Ionicons name="globe-outline" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="video"
         options={{
           title: 'Video',
+          tabBarIcon: ({ color, size }) => <Ionicons name="videocam-outline" size={size} color={color} />,
         }}
       />
     </Tabs>
