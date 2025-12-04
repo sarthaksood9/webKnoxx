b2e7cdce1e0da521aedf83a09539c94b4f62ca5d
sarthaksood9 <sarthaksood9@gmail.com>
2025-12-05 00:00:42 +0530

    fix icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
